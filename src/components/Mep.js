d03d9dcdf41ea832e450f481463ba83408fa5192
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2023-09-17 22:01:57 +0200

    Add files via upload

--- src/components/Mep.js
+++ src/components/Mep.js
@@ -1,8 +1,9 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 
 
 const Layout = () => {
+ 
   return (
     <section className="absolute h-screen w-screen grid grid-cols-1 grid-rows-5 gap-4">
       <div className="row-span-2 ">
@@ -16,7 +17,11 @@ const Layout = () => {
           <br /> dans le digital
         </h2>
       </div>
-     
+     <div className="row-span-1 row-start-5 flex justify-center">
+     <svg id="anim" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.3} stroke="currentColor" className="w-12 h-12">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
+</svg>
+      </div>
     </section>
   );
 };
