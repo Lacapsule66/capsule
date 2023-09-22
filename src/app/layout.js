cfdf301731a7eb3f221ed1289d16de778d052732
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2023-09-22 13:00:36 +0200

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,20 +58,20 @@ export default function RootLayout({ children }) {
         `}
       </Script>
       <body className="relative">
-<Time/>
+{/* <Time/> */}
 
-        <Suspense fallback={<div className='h-screen w-full absolute top-0 z-[200]'>
+        {/* <Suspense fallback={<div className='h-screen w-full absolute top-0 z-[200]'>
         <div className="h-screen relative  w-full flex justify-center items-center">
         <div id='ok' className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
         <p className='absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2'>Chargement ...</p>
         </div>
 
   
-        </div>}>
+        </div>}> */}
     <Header2 />
     {children}
     <Footer/>
-    </Suspense>
+    {/* </Suspense> */}
 
 
     <Analytics />
